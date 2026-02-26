993f425519aa478738f8551d6e742ec2148c7677
MarkKochRuge <markkochruge@gmail.com>
2026-02-26 21:05:28 +0100

    init

---
+++ FormattedError.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using FluentResults;
+
+namespace miniproject;
+
+public class FormattedError : IError
+{
+  public required string UnformattedMessage { get; set; }
+  public string Message { get => GetFormattedMessage(); set {} }
+  public Dictionary<string, Object> Metadata { get; set; } = [];
+  public List<IError>? Reasons { get; set; }
+
+  private string GetFormattedMessage()
+  {
+    var messageBuilder = new StringBuilder();
+    var messageEnumerator = UnformattedMessage.GetEnumerator();
+
+    while (messageEnumerator.MoveNext())
+    {
+      var currentChar = messageEnumerator.Current;
+
+      if (currentChar is '{')
+      {
+        var key = GetKey(messageEnumerator);
+        messageBuilder.Append(key);
+      }
+      else
+        messageBuilder.Append(currentChar);
+    }
+
+    var message = messageBuilder.ToString();
+    return message;
+  }
+
+  private string GetKey(IEnumerator<Char> messageEnumerator)
+  {
+    var keyBuilder = new StringBuilder();
+
+    while (messageEnumerator.MoveNext())
+    {
+      var currentChar = messageEnumerator.Current;
+
+      if (currentChar is '}')
+      {
+        var key = keyBuilder.ToString();
+
+        if (Metadata.TryGetValue(key, out var value))
+          return value.ToString() ?? string.Empty;
+        else
+          return '{' + key + '}';
+      }
+      else
+        keyBuilder.Append(currentChar);
+    }
+
+    return '{' + keyBuilder.ToString();
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using miniproject;
+
+var errorCode = "System Crash";
+
+var error = new FormattedError
+{
+    UnformattedMessage = $"an error occured: {{{nameof(errorCode)}}}",
+    Metadata =
+    {
+        {nameof(errorCode), errorCode}
+    }
+};
+
+var user = "USR_01";
+var errorMessage = "not authenticated";
+
+var error2 = new FormattedError
+{
+    UnformattedMessage = $"user: \"{{{nameof(user)}}}\" could not login due to: {{{nameof(errorMessage)}}}",
+    Metadata =
+    {
+        {nameof(user), user},
+        {nameof(errorMessage), errorMessage}
+    }
+};
+
+Console.WriteLine(error.Message);
+Console.WriteLine(error2.Message);
